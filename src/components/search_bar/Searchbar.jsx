4f75ca62cb9cfb3a1def742fbc72229c6ede2109
RZorkin <zorkin81@gmail.com>
2023-05-19 21:03:50 +0300

    Add project files

--- src/components/search_bar/Searchbar.jsx
+++ src/components/search_bar/Searchbar.jsx
@@ -1,3 +1,51 @@
+// import '../styles.css';
+// import { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import { FiSearch } from 'react-icons/fi';
+
+// const Searchbar = ({ onSubmit }) => {
+//   const [searchQuery, setSearchQuery] = useState('');
+
+//   const handleChange = ({ target: { value } }) => {
+//     setSearchQuery(value);
+//   };
+
+//   const restForm = ({ value }) => { 
+//     setSearchQuery("");
+//   };
+
+//   const handleOnSubmit = event => {
+//     event.preventDefault();
+//     onSubmit(searchQuery);
+//     restForm();
+//   };
+
+//   return (
+//     <header className="Searchbar">
+//       <form className="SearchForm" onSubmit={handleOnSubmit}>
+//         <button type="submit" className="SearchForm-button">
+//           <FiSearch />
+//         </button>
+//         <input
+//           className="SearchForm-input"
+//           onChange={handleChange}
+//           value={searchQuery}
+//           type="search"
+//           autoComplete="off"
+//           autoFocus
+//           placeholder="Search images and photos"
+//         />
+//       </form>
+//     </header>
+//   );
+// };
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
+
+// export default Searchbar;
+
 import '../styles.css';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
@@ -10,14 +58,14 @@ const Searchbar = ({ onSubmit }) => {
     setSearchQuery(value);
   };
 
-  const restForm = ({ value }) => { 
+  const resetForm = () => { 
     setSearchQuery("");
   };
 
   const handleOnSubmit = event => {
     event.preventDefault();
     onSubmit(searchQuery);
-    restForm();
+    resetForm();
   };
 
   return (
